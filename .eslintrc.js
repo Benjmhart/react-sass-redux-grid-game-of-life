e81b7d5fd696d2bbf71698d3cbb7ab7596912ad2
Ben Hart <Benjmhart@gmail.com>
2018-01-05 23:23:50 -0500

    integrated eslint/airbnb and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+    "extends": [ 
+        "airbnb",
+        "prettier/recommended"
+    ],
+    "plugins": [
+        "react",
+        "jsx-ally",
+        "import",
+        "prettier"
+    ],
+    "rules": {
+        "prettier/prettier": "error",
+        "react/jsx-filename-extension": 0
+    }
+};
