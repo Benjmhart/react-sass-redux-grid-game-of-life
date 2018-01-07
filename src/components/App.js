68032ad52fbe1ff9fd3ee070b7362434e7241af2
Ben Hart <Benjmhart@gmail.com>
2018-01-07 17:17:16 -0500

    removed svg

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,11 @@
 import React, { Component } from "react";
-import logo from "../logo.svg";
 import "../styles/App.css";
 
 class App extends Component {
+  constructor() {
+    super()
+  }
+  
   render() {
     return (
       <div className="App">
